81b3f97f21d12e7a65d9bb1a30ae8fdc304b180c
Evan Music <music.evan14@gmail.com>
2024-04-12 09:50:46 -0400

    Add fancy ps1

--- programs.nix
+++ programs.nix
@@ -25,7 +25,34 @@
       make-keys = "ssh-keygen -t ed25519";
     };
     initExtra = ''
-      PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+      PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\ $ '
+
+      GIT_PROMPT_FILE_NIXOS="/run/current-system/sw/share/bash-completion/completions/git-prompt.sh"
+      GIT_PROMPT_FILE_NIX="$HOME/.nix-profile/share/git/contrib/completion/git-prompt.sh"
+
+      if [[ -f "$GIT_PROMPT_FILE_NIXOS" ]]; then
+        source "$GIT_PROMPT_FILE_NIXOS"
+      elif [[ -f "$GIT_PROMPT_FILE_NIX" ]]; then
+        source "$GIT_PROMPT_FILE_NIX"
+      fi
+
+      update_prompt() {
+        PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]'
+
+        if [[ $(command -v __git_ps1) ]]; then
+          PS1_CMD1=$(__git_ps1 " (%s)")
+          PS1+="\[\e[38;5;247m\]''${PS1_CMD1}\[\e[0m\]"
+
+          if [ -n "$DIRENV_DIR" ]; then
+            PS1+="% " # prompt is % in direnv directories
+          else
+            PS1+="\\$ " # prompt is $ in non-direnv directories
+          fi
+        fi
+      }
+
+      PROMPT_COMMAND='update_prompt'
+
       # setup direnv
       eval "$(direnv hook bash)"
       # enables hgrex-cli tab-completion
