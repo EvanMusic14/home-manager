ecbec9dcc27e001f9c8f8ccd2dfe8e938009cca4
Evan Music <89477708+EvanMusic14@users.noreply.github.com>
2024-04-26 15:50:05 -0400

    Update home.nix

--- home.nix
+++ home.nix
@@ -27,9 +27,9 @@ in
     [[ -f /usr/bin/git ]] && toBeRemoved+="git "
     [[ -f /usr/bin/vim ]] && toBeRemoved+="vim "
 
-    [[ ! $toBeRemoved == "" ]] && echo "Warning: Run: sudo apt purge --auto-remove $toBeRemoved -y"
+    [[ ! $toBeRemoved == "" ]] && echo "Warning: There are apt packages installed that may conflict with nix packages" && echo "Run: sudo apt purge --auto-remove $toBeRemoved -y" | xargs
 
-    [[ -f /snap/bin/firefox ]] && echo "Warning: Run: sudo snap remove --purge firefox"
+    [[ -f /snap/bin/firefox ]] && echo "Warning: There are snap packages installed that may conflict with nix packages" && echo "Run: sudo snap remove --purge firefox"
   '';
   # After switch check if ssh keys exist if not create them
   home.activation.createSshKey = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
