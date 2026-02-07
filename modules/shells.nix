08ef5aa187d38bcecb9484b82a6314e821c50c74
EvanMusic14 <emutilities@tuta.com>
2026-02-07 19:40:26 +0000

    Add nixgl

--- modules/shells.nix
+++ modules/shells.nix
@@ -2,6 +2,7 @@
   config,
   lib,
   pkgs,
+  nixgl,
   ...
 }:
 
@@ -24,6 +25,8 @@ let
   };
 in
 {
+  nixGL.packages = nixgl.packages;
+
   # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.zsh.enable
   programs.zsh = {
     enable = true;
@@ -57,5 +60,34 @@ in
     enable = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
+  };
+
+  # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.kitty.enable
+  programs.kitty = {
+    package = config.lib.nixGL.wrap pkgs.kitty;
+    enable = true;
+    themeFile = "kanagawa";
+    shellIntegration = {
+      enableZshIntegration = true;
+    };
+    settings = {
+      active_border_color = "#44ffff";
+      background_blur = "64";
+      background_opacity = "0.9";
+      enabled_layouts = "Tall, *";
+      shell = "zsh";
+      single_windows_margin_width = 1;
+      tab_bar_style = "powerline";
+      tab_powerline_style = "slanted";
+      window_border_width = "1pt";
+      window_margin_width = "10";
+    };
+  };
+
+  # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.wezterm.enable
+  programs.wezterm = {
+    enable = true;
+    enableZshIntegration = true;
+    extraConfig = builtins.readFile ./configs/wezterm/wezterm.lua;
   };
 }
